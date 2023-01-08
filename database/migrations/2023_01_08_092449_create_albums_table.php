da86ed567793f8fe593cf4bdd248ac5416840504
m.kato <mkatochoco1013@gmail.com>
2023-01-08 19:04:46 +0900

    [Add] マイグレーションファイル追加

---
+++ database/migrations/2023_01_08_092449_create_albums_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id()->comment('アルバムID');
+            $table->string('name')->comment('アルバム名');
+            $table->timestamps();
+            $table->tinyInteger('is_private')->comment('外部非公開アルバムフラグ');
+            $table->unsignedBigInteger('user_id')->comment('作成者ID');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+
+        DB::statement("ALTER TABLE albums COMMENT 'アルバムテーブル'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+};
