3324d6ebb533dc7ef8437ebd50aef791b2a6c352
m.kato <mkatochoco1013@gmail.com>
2023-01-09 14:43:07 +0900

    [Add] photosテーブル - migrationファイル追加

---
+++ database/migrations/2023_01_09_051643_create_photos_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('画像名');
+            $table->text('description')->comment('画像説明');
+            $table->unsignedBigInteger('album_id')->comment('アルバムID');
+            $table->timestamps();
+            $table->tinyInteger('is_private')->comment('外部非公開画像フラグ');
+            $table->unsignedBigInteger('user_id')->comment('投稿者ID');
+
+            $table->foreign('album_id')
+            ->references('id')
+            ->on('albums')
+            ->cascadeOnDelete()
+            ->cascadeOnUpdate();
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->cascadeOnDelete()
+            ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+};
