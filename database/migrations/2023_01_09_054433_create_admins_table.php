1d404e4ab5f908b9a5065bc01356a029c8600226
m.kato <mkatochoco1013@gmail.com>
2023-01-09 14:47:59 +0900

    [Add] adminテーブル - migrationファイル追加

---
+++ database/migrations/2023_01_09_054433_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id()->comment('管理者ID');
+            $table->string('name')->comment('管理者名');
+            $table->string('password')->comment('管理者パスワード');
+            $table->timestamp('login_date')->nullable()->comment('最終ログイン日時');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
