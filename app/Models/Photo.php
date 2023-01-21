6eeade742615c514658f5460150f7b2009d8f088
m.kato <mkatochoco1013@gmail.com>
2023-01-21 13:42:18 +0900

    [Add] トランザクション、リレーション追記

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class Photo extends Model
@@ -44,6 +45,22 @@ class Photo extends Model
     ];
 
     /**
+     * アルバムとのリレーション
+     */
+    public function album()
+    {
+        return $this->hasOne(Album::class);
+    }
+
+    /**
+     * ユーザーとのリレーション
+     */
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    /**
      * 一件取得
      */
     public static function getList($album_id)
@@ -57,38 +74,46 @@ class Photo extends Model
      */
     public static function upsert($request, $album_id, $id = null)
     {
-        // DB::transaction();
-        if ($id) {
-            // 更新
+        DB::transaction();
+        try {
+            if ($id) {
+                // 更新
 
-        } else {
-            // 登録
-            $instance = new Photo();
-            $instance->name = $request->name;
-            $instance->description = $request->description;
-            $instance->album_id = $album_id;
-            $instance->is_private = $request->is_private;
-            $instance->user_id = Auth::id();
-            $instance->save();
-            if (isset($request->photo_img)) {
-                // 画像ファイル情報取得
-                $extension = pathinfo($_FILES['photo_img']['name'], PATHINFO_EXTENSION);
-                $instance->extension = $extension;
+            } else {
+                // 登録
+                $instance = new Photo();
+                $instance->name = $request->name;
+                $instance->description = $request->description;
+                $instance->album_id = $album_id;
+                $instance->is_private = $request->is_private;
+                $instance->user_id = Auth::id();
+                $instance->save();
+                if (isset($request->photo_img)) {
+                    // 画像ファイル情報取得
+                    $extension = pathinfo($_FILES['photo_img']['name'], PATHINFO_EXTENSION);
+                    $instance->extension = $extension;
 
-                // 画像S3保存
-                $file = $request->file('photo_img');
-                $image_path = '/albums' . '/' . $album_id . '/photos' . '/' . $instance['id'];
-                $image_name = 'photo_img' . '.' . $extension;
-                $photo_img_info = Storage::putFileAs(
-                    $image_path,
-                    $file,
-                    $image_name
-                );
-                $instance->photo_img = Storage::url($photo_img_info);
+                    // 画像S3保存
+                    $file = $request->file('photo_img');
+                    $image_path = '/albums' . '/' . $album_id . '/photos' . '/' . $instance['id'];
+                    $image_name = 'photo_img' . '.' . $extension;
+                    $photo_img_info = Storage::putFileAs(
+                        $image_path,
+                        $file,
+                        $image_name
+                    );
+                    $instance->photo_img = Storage::url($photo_img_info);
+                }
+                $instance->update();
+                // 二重送信防止
+                $request->session()->regenerateToken();
             }
-            $instance->update();
-            // 二重送信防止
-            $request->session()->regenerateToken();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            Log::error($e->getMessage());
+            Log::error($e->getFile());
+            Log::error($e->getLine());
         }
     }
 
